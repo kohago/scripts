29dc70ed014bf16f057981e4f51670ceef8a5c16
kouha-shu <kouha.shu@gmail.com>
2025-08-02 15:46:31 +0900

    Update install_phpenv.sh

--- install_phpenv.sh
+++ install_phpenv.sh
@@ -14,5 +14,5 @@ source ~/.zshrc
 
 #php-build
 git clone https://github.com/php-build/php-build $(phpenv root)/plugins/php-build
-brew install zlib bzip2 curl libiconv libedit openssl@1.1
-echo 'export PHP_BUILD_CONFIGURE_OPTS="--with-zlib-dir=$(brew --prefix zlib) --with-bz2=$(brew --prefix bzip2) --with-curl=$(brew --prefix curl) --with-iconv=$(brew --prefix libiconv) --with-libedit=$(brew --prefix libedit) --with--ssl=$(brew --prefix openssl@1.1)"' >> ~/.zshrc
+brew install zlib bzip2 curl libiconv libedit openssl
+echo 'export PHP_BUILD_CONFIGURE_OPTS="--with-zlib-dir=$(brew --prefix zlib) --with-bz2=$(brew --prefix bzip2) --with-curl=$(brew --prefix curl) --with-iconv=$(brew --prefix libiconv) --with-libedit=$(brew --prefix libedit) --with--ssl=$(brew --prefix openssl)"' >> ~/.zshrc
